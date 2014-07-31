50688c64391817170312b136a56d0b6943ea6882
Dexter <dexter@launchlabs.ph>
2014-07-31 17:28:28 +0800

    Swipesv3

---
+++ Swipes/Swipes/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Swipes
+//
+//  Created by Dexter Launchlabs on 7/31/14.
+//  Copyright (c) 2014 Dexter Launchlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label; @property CGPoint gestureStartPoint;
+@end
